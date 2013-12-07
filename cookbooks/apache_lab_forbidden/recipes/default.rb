3a0c5f8fe9687db4384c682127dc58ce734dae7b
Rocco Muscaritolo <rocco.muscaritolo@rackspace.com>
2013-12-06 20:41:13 -0500

    Working on first lab...

--- cookbooks/apache_lab_forbidden/recipes/default.rb
+++ cookbooks/apache_lab_forbidden/recipes/default.rb
@@ -13,7 +13,6 @@ when "redhat","centos"
   execute "rm -rf /etc/httpd/vhost.d/*" do
     only_if "test -d /etc/httpd/vhost.d/"
   end
-end
 
 when "ubuntu","debian"
   execute "rm -rf /etc/apache2/sites-enabled/*" do
@@ -28,21 +27,51 @@ when "redhat","centos"
     owner "root"
     group "root"
   end
+
+  template "/etc/httpd/conf/httpd.conf" do
+    source "httpd.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+
+  template "/etc/httpd/vhost.d/001-default.conf" do
+    source "001-default.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+
+when "ubuntu","debian"
+  template "/etc/apache2/apache2.conf" do
+    source "apache2.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+
+  template "/etc/apache2/sites-available/001-default.conf" do
+    source "001-default-ubuntu.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+
+  execute "a2ensite 001-default.conf" 
 end
 
 
-template "/etc/httpd/vhost.d/001-default.conf" do
-  source "001-default.conf.erb"
-  owner "apache"
-  group "apache"
-  mode 0644
-end
-
-
-
+case node[:platform]
+when "redhat","centos"
 service "httpd" do
   action :restart
 end
+when "ubuntu","debian"
+service "apache2" do
+  action :restart
+end
+end
+
 
 
 
